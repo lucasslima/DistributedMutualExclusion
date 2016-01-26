fdd7fe2e70a2f536ee1bddad9d50d73ec9badeb7
Lucas Lima <lima.lucas.ma@gmail.com>
2016-01-25 23:47:54 -0200

    Added preliminary code on Client

--- src/appl/Client.java
+++ src/appl/Client.java
@@ -5,24 +5,32 @@ import java.io.BufferedOutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.util.Random;
+import java.util.function.Consumer;
 
 import server.JCL_message;
 import server.JCL_messageImpl;
 
 public class Client {
 
+	private int feedCount;
+	private int id;
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		new Client();
+		new Client(Integer.parseInt(args[1]));
 	}
 	
-	public Client(){
+	public Client(int id){
+		feedCount = 0;
 		try{
 			Socket s = new Socket("localhost", 6969);
-						
+			while (true){
+				Thread.sleep( (long) Math.random() % 1000);
+				
+			}
 		}catch(Exception e){
 			e.printStackTrace();
 		}
--- src/server/JCL_messageImpl.java
+++ src/server/JCL_messageImpl.java
@@ -1,15 +1,20 @@
 package server;
 
+import java.sql.Timestamp;
+
 public class JCL_messageImpl implements JCL_message{
 
 	/**
 	 * 
 	 */
+	public static int ACK = 0;
+	public static int RELEASE = 1;
 	private static final long serialVersionUID = -1298874117877687170L;
 	private int type;
-	
+	private Timestamp mTimestamp;
+	private int message;
 	public JCL_messageImpl(){
-		
+		mTimestamp = new Timestamp(System.currentTimeMillis());
 	}
 	
 	@Override
